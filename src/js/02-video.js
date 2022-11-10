b3a733e2dcd853d1548b12dafc2467be6da6cb44
npakypop <leaterood@gmail.com>
2022-11-10 23:08:26 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const throttle = require("lodash.throttle");
+
+const TIME_KEY = "videoplayer-current-time";
+
+
+player.on('timeupdate', throttle(pushToLocalStorage, 1000));
+
+function pushToLocalStorage(event) {  
+    localStorage.setItem(TIME_KEY, event.seconds); 
+}
+
+player.setCurrentTime(localStorage.getItem(TIME_KEY))
+
