13d5b035ab73230b4f2de97889e1f02bec09185f
npakypop <leaterood@gmail.com>
2022-11-13 13:50:56 +0200

    fix after check

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,27 @@ import storageApi from './storage';
 var throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('submit', throttle(onSubmitBtn, 250));
+form.addEventListener('submit', onSubmitBtn);
 form.addEventListener('input', throttle(onFieldInput, 250));
 
 const USER_DATA = "feedback-form-state";
 
-const userInput = {};
+
+function fillInputFields() { 
+   
+    const a = storageApi.load(USER_DATA)
+    
+    for (const key in a) {
+        if (a.hasOwnProperty(key)){ 
+            form.elements[key].value = a[key];
+        }
+    }
+}
+fillInputFields()
+
 
 function onFieldInput(event) {
+    const userInput = localStorage.getItem(USER_DATA) ? storageApi.load(USER_DATA) : {};
     const { target } = event;
 
     const name = target.name;
@@ -21,19 +34,16 @@ function onFieldInput(event) {
     // localStorage.setItem('USER_DATA', JSON.stringify(userInput));
 }
 
-function fillInputFields() { 
-    const a = storageApi.load(USER_DATA)
-    
-    for (const key in a) {
-        if (a.hasOwnProperty(key)){ 
-            form.elements[key].value = a[key];
-        }
-    }
-}
-fillInputFields()
 
 function onSubmitBtn(event) { 
     event.preventDefault();
+    const formData = new FormData(form);
+    for (const [, value] of formData) { 
+        if (!value) { 
+            alert('Please fill all the fields');
+            return;
+        }
+    }
     console.log(JSON.parse(localStorage.getItem(USER_DATA)));
     event.target.reset();
     storageApi.remove(USER_DATA);
